3cd65e2a38fff61c47ec4cbaa76032e74d9419e5
elisha <namelisha@gmail.com>
2023-05-05 19:45:03 -0700

    Created the 4 basic calculator functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log("1 + 2", add(1, 2));
+console.log("2 - 1", subtract(2, 1));
+console.log("1 - 2", subtract(1, 2));
+console.log("4 * 5", multiply(4, 5));
+console.log("0.2 * 4", multiply(0.2, 4));
+console.log("20 / 4", divide(20, 4));
+console.log("4 / 20", divide(4, 20));
